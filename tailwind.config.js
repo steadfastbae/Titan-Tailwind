ed42fc24fb88ce5bce56e47e76d419c6572df5fa
shawn <steadfastbae@gmail.com>
2023-03-12 20:11:25 -0500

    samplecode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./layout/*.liquid",
-    "./sections/*.liquid",
-    "./snippets/*.liquid",
-    "./templates/customers/*.liquid",
-    "./templates/*.liquid"
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
